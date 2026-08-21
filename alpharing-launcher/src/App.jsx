0195c34d9a627749586202836a81931d332f7a42
Jack Bittner <jack.bittner@outlook.com>
2026-08-21 11:31:41 -0500

    feat: repository cleanup

--- alpharing-launcher/src/App.jsx
+++ alpharing-launcher/src/App.jsx
@@ -3,24 +3,11 @@ import { invoke } from "@tauri-apps/api/core";
 import { listen } from "@tauri-apps/api/event";
 import { getCurrentWindow } from "@tauri-apps/api/window";
 import alphaRingLogo from "./assets/logo_alpharing.png";
-import johnHalo from "./assets/john_halo.png";
-// import bkgVideo from "./assets/bkgVideo.mp4";
 import bkgImage from "./assets/bkgImage.png";
-// import selectSound from "./assets/select.wav";
-// import clickSound from "./assets/click.wav";
 import "./App.css";
 
 const CLOSE_DELAY_MS = 1000;
 const ERROR_DISPLAY_MS = 2000;
-
-const playSound = (src) => {
-  // new Audio(src).play().catch(() => {});
-};
-
-const playSelectSound = () => {
-  // selectAudio.currentTime = 0;
-  // selectAudio.play().catch(() => {});
-};
 
 const closeWindowDelayed = () => {
   setTimeout(() => getCurrentWindow().close(), CLOSE_DELAY_MS);
@@ -73,7 +60,6 @@ function App({buildInfo = "", modInfo: initialModInfo = ""}) {
 
   const changeSelection = (index) => {
     setSelectedIndex((prev) => {
-      if (prev !== index) playSelectSound();
       return index;
     });
   };
@@ -200,7 +186,6 @@ function App({buildInfo = "", modInfo: initialModInfo = ""}) {
   return (
     <main className="container">
       <div className="background" ref={backgroundRef}>
-        {/* <video className="background-video" src={bkgVideo} autoPlay loop muted playsInline/> */}
         <img className="background-video" src={bkgImage}/>
         <img className="logo" src={alphaRingLogo}/>
         {showLog ? (
